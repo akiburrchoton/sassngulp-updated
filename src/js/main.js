33d4b5519f8fca629a25844d1b9b0235bff4f635
Akibur <akibur.choton@gmail.com>
2021-02-14 18:03:07 +0600

    updated - sliders added and customised

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+var elem = document.querySelector('.slider-container');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  fade: true,
+  draggable: false,
+  autoPlay: 123456,
+  pageDots: false,
+  pauseAutoPlayOnHover: false,
+});
+
